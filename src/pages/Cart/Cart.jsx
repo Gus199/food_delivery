a33dec4127c2c4e61ac725d49e943a37b25d04e3
Amar <augustine2903a@gmail.com>
2024-07-13 12:33:53 -0400

    cart total and styling it

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -20,12 +20,53 @@ const Cart = () => {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div className='cart-items-title cart-items-item'>
-                <p>{item.name}</p>
+              <div key={index}>
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt='' />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>
+                    x
+                  </p>
+                </div>
+                <hr />
               </div>
             );
           }
         })}
+      </div>
+      <div className='div cart-bottom'>
+        <div className='cart-total'>
+          <h2>Cart Total</h2>
+          <div>
+            <div className='cart-total-details'>
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className='cart-total-details'>
+              <p>Deliovery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className='cart-total-details'>
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHEECKOUT</button>
+        </div>
+        <div className='cart-promocode'>
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className='cart-promocode-input'>
+              <input type='text' placeholder='promo code' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
